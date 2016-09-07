496ee65f2e5f0d6ed7c092b8a8c3a63df98a7264
Abhiram E <abhis3798@gmail.com>
2016-09-07 00:06:43 -0400

    Changes to allow mustache integration to render html

---
+++ scripts/popup.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded',main());
+
+function main()
+{
+	console.log("JS loaded.");
+	var xhr= new XMLHttpRequest();
+	xhr.open("GET","https://www.reddit.com/.json",false);
+	xhr.send();
+	console.log(xhr.status);
+	if(xhr.status===200)
+	{
+		var response= JSON.parse(xhr.response);
+		console.log(response.data.children);
+		var children=response.data.children;
+		document.addEventListener('DOMContentLoaded',function(){
+			var template=document.getElementById('content').innerHTML;
+			var render=Mustache.to_html(template,{title:children[0].data.title,
+				url:children[0].data.url});
+			document.getElementById("content").innerHTML=render;
+			console.log(document);
+		});
+	}
+
+}
+
